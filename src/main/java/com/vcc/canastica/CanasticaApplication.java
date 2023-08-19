30d48d1a2b3f4b41a32762799eb0354a36ca155b
nilcevc <nvilac@ucenfotec.ac.cr>
2023-08-19 02:31:43 -0600

    todo

--- src/main/java/com/vcc/canastica/CanasticaApplication.java
+++ src/main/java/com/vcc/canastica/CanasticaApplication.java
@@ -6,10 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.security.Security;
+
 @SpringBootApplication
 public class CanasticaApplication {
 
     public static void main(String[] args) {
+        Security.setProperty("jdk.tls.disabledAlgorithms","SSLv3, RC4, DES, MD5withRSA, DH keySize < 1024,EC keySize < 224, 3DES_EDE_CBC, anon, NULL");
         SpringApplication.run(CanasticaApplication.class, args);
     }
     @Bean
@@ -21,4 +24,5 @@ public class CanasticaApplication {
             }
         };
     }
+
 }
